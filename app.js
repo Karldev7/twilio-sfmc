44f1cbc735bec97976209b06e4c722c911f108c5
Karldev7 <slslove7@gmail.com>
2021-09-27 11:35:51 +0900

    09271135

--- app.js
+++ app.js
@@ -9,12 +9,18 @@ var path        = require('path');
 var request     = require('request');
 var routes      = require('./routes');
 var activity    = require('./routes/activity');
+var url=require('url');
+
+require('request').debug = true;
+
+console.log('H02');
 
 var app = express();
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.json({type: 'application/json'})); 
+
+app.use(bodyParser.raw({type: 'application/jwt'}));
 //app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(express.methodOverride());
@@ -27,7 +33,17 @@ if ('development' == app.get('env')) {
   app.use(errorhandler());
 }
 
-// HubExchange Routes
+console.log('INFLOW START ------------------------------------------------------------------------------');
+
+app.use((req, res, next) => {
+  //console.log('%s', req);
+  console.log('PORT: '+req.PORT);
+  next();
+ });
+
+ console.log('INFLOW END ------------------------------------------------------------------------------');
+
+ // HubExchange Routes
 app.get('/', routes.index );
 app.post('/login', routes.login );
 app.post('/logout', routes.logout );
@@ -38,6 +54,32 @@ app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
+//app.post('/journeybuilder/execute/', console.log('HERE99') );
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+
+  app.use((req, res, next) => {
+    var pathname=url.parse(req.url).pathname;
+    console.log('Pathname: '+pathname);
+  /*
+    switch(pathname){
+        case '/routes/activity.js':
+            res.end('activity');
+        break;
+        case '/public/js/customActivity.js':
+            res.end('customActivity');
+        break;
+        case '/journeybuilder/save/':
+            res.end('/journeybuilder/save/');
+        break;
+        default:
+            res.end('default');
+        break;
+    }
+*/    
+      next();
+   });
 });
+
